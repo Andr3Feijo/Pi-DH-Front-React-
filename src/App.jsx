8d70a507d796940780e5e521979eb28d5bc90553
André Feijó <107944135+Alsf30@users.noreply.github.com>
2023-05-31 21:02:05 -0300

    inserindo home no index e alterando app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 
+import { Router } from 'react-router-dom';
 import './App.css';
 import Header from './components/Header';
 
 function App() {
   return (
     <div className="App">
-      <Header />
+      <Router/>
     </div>
   );
 }
