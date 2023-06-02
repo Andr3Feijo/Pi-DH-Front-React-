bd3705ce7b7cfa7f706b7356660de1da7c3b87ef
André Feijó <107944135+Alsf30@users.noreply.github.com>
2023-06-01 21:13:06 -0300

    Trabalhando no Header em andamento

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,14 @@
 import { Router } from 'react-router-dom';
 import './App.css';
 import Header from './components/Header';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div className="App">
       <Router/>
+      <Header/>
+      <Footer/>
     </div>
   );
 }
--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,22 +1,77 @@
 import React from "react"
-import logo from '../../logo.svg';
+import style from './style.css'
 
 const Header = () => {
+  
+ document.addEventListener('click',function(e){
+  // Hamburger menu
+  if(e.target.classNameList.contains('hamburger-toggle')){
+    e.target.children[0].classNameList.toggle('active');
+  }
+})   
   return (
-    <header className="App-header">
-    <img src={logo} className="App-logo" alt="logo" />
-    <p>
-      Seja Bem-Vindos ao React!
-    </p>
-    <a
-      className="App-link"
-      href="https://reactjs.org"
-      target="_blank"
-      rel="noopener noreferrer"
-    >
-      Learn React
-    </a>
-  </header>
+   <>
+      <header className="header ">
+      <div className="div-topo">
+        <div className="">
+          <a href="/">
+            <img src="/images/I-SPACE logo oficial.jpg" alt="" id="img-logo"/>
+
+          </a>
+          
+        </div>
+        <form className="form-header">
+          <div className="input-pesquisar">
+              <input className="" type="search" placeholder="Pesquise" aria-label="Search"/>
+              <button className="button-send" type="submit">
+                <p>Buscar</p>
+                </button>
+          </div>
+  
+      </form>
+      <div className="div-user">
+      <li className="">
+        <a className="" href="users/create">Cadastro</a>
+      </li>
+      <li className="">
+        <a className="" href="../login">Login</a>
+      </li>
+      </div>
+      </div>
+     
+      <nav className="div-navbar">
+              <ul className="ul-nav">
+                <li className="/">
+                  <a className="" href="/">Inicio</a>
+                </li>
+
+                  <li className="">
+                    <a className="" href="/productPage/iphone">Iphone</a>
+                  </li>
+                  <li className="">
+                    <a className="" href="/productPage/mac">Mac</a>
+                  </li>
+                  <li className="">
+                    <a className="" href="/productPage/ipad">Ipad</a>
+                  </li>
+                  
+                <li className="">
+                  <a className=""  href="/productPage/watch">Watch</a>
+                </li>
+                <li className="">
+                  <a className=""  href="/sobre">Sobre</a>
+                </li>
+                <li className="">
+                  <a className=""  href="/">Serviços</a>
+                </li> 
+                <li className="">
+                  <a className=""  href="/estoque">Estoque</a>
+                </li>
+              </ul>  
+        </nav>
+
+   </header>
+   </>
   )
 }
 
